15553448209d41b93fc861bb9f0529deaeda0ef3
git-amruta <amrutajoshi20@gmail.com>
2022-11-02 16:50:40 +0530

    new added

--- 01-Springboot-FileUploading/src/main/java/com/cjc/in/Application.java
+++ 01-Springboot-FileUploading/src/main/java/com/cjc/in/Application.java
@@ -11,6 +11,7 @@ public class Application {
  		System.out.println("Main Running successfully");
 		SpringApplication.run(Application.class, args);
 		System.out.println("File Uplaoding Apllication Reunning");
+               System.out.println("Amruta added fourth line 2");
 	}
 
 }
